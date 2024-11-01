16851f6be1beb4ec1f4d3dac475269a6e6654269
Rahma226 <sherifrahma829@gmail.com>
2024-11-01 13:07:15 +0200

    add image card to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:readify/core/utils/assests.dart';
 import 'package:readify/features/home/presentation/views/widgets/book_details_view_widgets/custom_app_bar.dart';
+import 'package:readify/features/home/presentation/views/widgets/home_view_widgets/image_card.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-            CustomAppBarBookDetails()
+           const CustomAppBarBookDetails(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .17),
+              child:const ImageCard(image: Asset.image3),
+            ),
         ],
       ),
     );

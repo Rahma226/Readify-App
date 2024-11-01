dc2f7e88fe3add9d825b5c39174faffa2ae4b189
Rahma226 <sherifrahma829@gmail.com>
2024-11-01 14:16:42 +0200

    add rating to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:readify/core/utils/assests.dart';
 import 'package:readify/core/utils/style.dart';
 import 'package:readify/features/home/presentation/views/widgets/book_details_view_widgets/custom_app_bar.dart';
+import 'package:readify/features/home/presentation/views/widgets/home_view_widgets/book_rating.dart';
 import 'package:readify/features/home/presentation/views/widgets/home_view_widgets/image_card.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -35,6 +36,12 @@ class BookDetailsViewBody extends StatelessWidget {
               'J.K. Rowling',
               style: Style.textStyle18.copyWith(fontStyle: FontStyle.italic),
             ),
+          ),
+           const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/features/home/presentation/views/widgets/home_view_widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/home_view_widgets/book_rating.dart
@@ -4,30 +4,34 @@ import 'package:readify/constants.dart';
 import 'package:readify/core/utils/style.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({super.key, this.mainAxisAlignment= MainAxisAlignment.start});
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
           color: Color(0xffFFDD4F),
-          size: 16,
+          size: 14,
         ),
         const SizedBox(
           width: 6.3,
         ),
         const Text(
           '4.8',
-          style: Style.textStyle14,
+          style: Style.textStyle16,
         ),
         const SizedBox(
           width: 5,
         ),
-        Text(
-          '(2544)',
-          style: Style.textStyle14.copyWith(color: Kcolor2),
+        const Opacity(
+          opacity: .5,
+          child: Text(
+            '(2544)',
+            style: Style.textStyle14,
+          ),
         )
       ],
     );

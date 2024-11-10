0891354bdc6ee75757f400288ef4a602749dfd4a
Rahma226 <sherifrahma829@gmail.com>
2024-11-10 15:04:57 +0200

    create book model

--- lib/features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:readify/constants.dart';
 
 class CustomSearchTextField extends StatelessWidget {
   const CustomSearchTextField({super.key});
@@ -10,21 +9,21 @@ class CustomSearchTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
       style: TextStyle(color: Colors.white),
-      cursorColor: KsecondryColor,
+      cursorColor: Colors.white,
       decoration: InputDecoration(
         filled: true,
         fillColor: const Color.fromARGB(255, 30, 28, 28),
         hintText: 'Search...',
         hintStyle: TextStyle(color: Colors.white60),
-        prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass,size: 16,
-            color: Colors.white), 
+        prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass,
+            size: 16, color: Colors.white),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32), 
-          borderSide: BorderSide.none, 
+          borderRadius: BorderRadius.circular(32),
+          borderSide: BorderSide.none,
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(32),
-          borderSide: BorderSide(color: KsecondryColor),
+          borderSide: BorderSide(color: Colors.white),
         ),
       ),
     );

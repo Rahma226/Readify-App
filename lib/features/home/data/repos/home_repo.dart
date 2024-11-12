1d37a40e52ba9e6b0b85cc0b8bd9ffa57dbc3a6f
Rahma226 <sherifrahma829@gmail.com>
2024-11-12 13:38:38 +0200

    create server failure from dio error

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:readify/core/errors/failure.dart';
 import 'package:readify/features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
- Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>> fetchNewestBooks();
  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,13 +1,37 @@
+import 'dart:math';
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:readify/core/errors/failure.dart';
+import 'package:readify/core/utils/api_service.dart';
 import 'package:readify/features/home/data/models/book_model/book_model.dart';
 import 'package:readify/features/home/data/repos/home_repo.dart';
 
-class HomeRepoImplementation implements HomeRepo{
+class HomeRepoImplementation implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplementation(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+    }
+    return left(
+      ServerFailure(e.toString()),
+    );
   }
 
   @override
